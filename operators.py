e4fa2df44231f85c2afaac61e1e2b1aa42649350
R.Mowleeswari <aneeshmowleeswari@gmail.com>
2025-09-29 14:17:32 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,20 @@
+a=int(input("Enter first number="))
+b=int(input("Enter second number="))
+# addition 
+c=(a+b)
+print("addition of a numbers=",c)
+#subraction 
+c=(a-b)
+print("subraction of a numbers=",c)
+#multiplication 
+c=(a*b)
+print("multiplication of a numbers=",c)
+#division 
+c=(a/b)
+print("division of numbers=",c)
+#modulus
+c=(a%b)
+print("modulus of a numbers=",c)
+# floor division
+c=(a//b)
+print("floor division of a number=",c)
---
+++ strinconcadination.py
@@ -0,0 +1,4 @@
+str1=input("Enter your First Name=")
+str2=input("Enter your Second name=")
+strcon=str1+str2
+print("string concatenation:",strcon)
