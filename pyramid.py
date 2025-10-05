74c38f1a18bf8f3eed8a0a7fa5b5c6dca1dc7763
R.Mowleeswari <aneeshmowleeswari@gmail.com>
2025-10-05 10:05:20 +0530

    Add files via upload

---
+++ pyramid.py
@@ -0,0 +1,13 @@
+# n=5
+# for i in  range(n+1):
+#     for j in range(i,0,-1):
+#         print(j,end=" ")
+#     print(" ")
+
+
+
+n=5
+for i in range(n+1):
+    for j in range (i,0,-1):
+     print(j,end=" ")
+    print(" ")
