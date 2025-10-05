911ad069ea018c465c10895d58f44cf177e7948a
R.Mowleeswari <aneeshmowleeswari@gmail.com>
2025-10-05 10:05:48 +0530

    Add files via upload

---
+++ python calculator.py
@@ -0,0 +1,28 @@
+
+# operator=input(" +,-,%,*,**,//,/ :")
+# a=int(input("enter a 1st number="))
+# b=int(input("enter a 2nd nnumber="))
+# if operator=="+":
+#     print(a+b)
+# elif operator=="_":
+#     print(a-b)
+# elif operator=="%":
+#     print(a/b)
+# elif operator=="*":
+#     print(a*b)
+# elif operator=="**":
+#     print(a**b)
+# elif operator=="//":
+#     print(a//b)
+# elif operator=="/":
+#     print(a/b)
+# else:
+#     result="invalid operation."
+#     print("Result:",result)
+
+
+
+
+
+
+operator=input("+,-,*,//,/,")
